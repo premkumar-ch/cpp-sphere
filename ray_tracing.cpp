17194c546172f7992d24af2f35f0813e8e74d025
premkumar-ch <prem@billbox.co.in>
2026-05-18 11:10:34 +0530

    first commit

---
+++ ray_tracing.cpp
@@ -0,0 +1,83 @@
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+#include <iostream>
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
+color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(1, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (-unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+    // linear blend 
+    // blend value = (1-a) x start_value + a x end_value 
+int main() {
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = int(image_width / aspect_ratio);
+
+    image_height = (image_height < 1) ? 1 : image_height;
+    // view port is an invisible window	infront of camera
+        //view port should have same aspect ratio of image else the image looks streched
+    /*
+     in 3D coordinate system z axis is simply the distance between your eye and screen assume
+         or simple can be assumed view port
+    */
+
+
+    //Cameraauto 
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+    auto viewport_upper_left = camera_center
+        - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    std::cout << "P3\n";
+    std::cout << image_width << ' ' << image_height << '\n';
+    std::cout << "255\n";
+    // Paint
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScaling image "
+            << (image_height - j)
+            << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto pixel_center =
+                pixel00_loc
+                + (i * pixel_delta_u)
+                + (j * pixel_delta_v);
+            auto ray_dir = pixel_center - camera_center;
+            ray r(camera_center, ray_dir);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+    std::clog << "\n    Done       ";
+    return 0;
+}
---
+++ vec3.h
@@ -0,0 +1,112 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+class vec3 {
+public:
+    double e[3];
+
+    vec3() : e{ 0,0,0 } {}
+    vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
+
+    vec3& operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3& operator/=(double t) {
+        return *this *= 1 / t;
+    }
+
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+};
+
+// point3 is just an alias for vec3, but useful for geometric clarity in the code.
+using point3 = vec3;
+
+
+// Vector Utility Functions
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0]
+        + u.e[1] * v.e[1]
+        + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v) {
+    return v / v.length();
+}
+
+#endif
+/*
+cross produc of two vectors say A and B
+
+A = ai + bj + ck
+B = xi + yj + zk
+i x j = k ; j x k = i ; k x i = j;      X x X = 0 X E {i,j,k}
+}
+j x i = -k; k x j = -i; i x k = -j
+A × B = (ai + bj + ck) × (xi + yj + zk)
+
+= ax(i × i) + ay(i × j) + az(i × k) + bx(j × i) + by(j × j) + bz(j × k) + cx(k × i) + cy(k × j) + cz(k × k)
+A × B = (bz – cy)i – (az – cx)j + (ay – bx)k = (bz – cy)i + (cx – az)j + (ay – bx)k
+*/
